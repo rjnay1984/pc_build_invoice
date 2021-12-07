6daf3cd62099173949fbb3b60be8c7e1f9c81e19
RJ Nay <rjnay1984@gmail.com>
2021-12-07 16:56:32 -0600

    adding api framework

--- backend/invoice/views.py
+++ backend/invoice/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import InvoiceSerializer
+
+from .models import Invoice
 
 # Create your views here.
+class InvoiceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows invoices to be viewed.
+    """
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
