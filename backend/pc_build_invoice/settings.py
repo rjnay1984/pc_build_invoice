f7463c0553a1471a5d7786ec0e4db6e0dc114012
RJ Nay <rjnay1984@gmail.com>
2021-12-07 14:11:18 -0600

    add docker files

--- backend/pc_build_invoice/settings.py
+++ backend/pc_build_invoice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m$f2w2z!aj_1o%yv=*xd%!-%wh_i@io0fnu==j7(-2j91@581$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pc_build_invoice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pc_build',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
