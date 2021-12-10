b07d6f87bad7884fa6a197895a7c614a55143cc9
RJ Nay <rjnay1984@gmail.com>
2021-12-10 14:55:26 -0600

    remove css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <span className="navbar-brand">Pc Build Invoice</span>
+          <ul>
+            <li>
+              <a href="#">Login</a>
+            </li>
+            <li><a href="#">Register</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
