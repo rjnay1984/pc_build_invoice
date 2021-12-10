02dc711344686d958241b6b6365dbe5948580b3f
RJ <rjnay1984@gmail.com>
2021-12-09 20:30:35 -0600

    Rest api (#2)
    * adding api framework
    
    * formatting
    
    * add auth, update model/serializer
    
    * user relationship

---
+++ backend/invoice/serializers.py
@@ -0,0 +1,12 @@
+from django.db.models import fields
+from rest_framework import serializers
+
+from .models import Invoice
+
+
+class InvoiceSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.email')
+
+    class Meta:
+        model = Invoice
+        fields = '__all__'
--- backend/invoice/views.py
+++ backend/invoice/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import InvoiceSerializer
+
+from .models import Invoice
 
 # Create your views here.
+
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows invoices to be viewed/added/updated/deleted.
+    """
+    serializer_class = InvoiceSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Invoice.objects.filter(owner=user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
